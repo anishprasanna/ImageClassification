6e7bf057c65484d8531435fe0eb82c728bdc7e39
Carlos Samaniego <cscarlossamaniego@gmail.com>
2019-09-13 00:20:24 -0400

    added features

--- imageProcessing_2/image_processing.py
+++ imageProcessing_2/image_processing.py
@@ -1,15 +1,17 @@
 import glob
 import random
 import os
+import shutil
 
 def main():
     #collect list of file paths from cats and dogs
-    cat_photos = glob.glob('/Users/Carlos/Projects/Data_Mining/imageProcessing/photos/cats' + '/*.jpg')
-    dog_photos = glob.glob('/Users/Carlos/Projects/Data_Mining/imageProcessing/photos/dogs' + '/*.jpg')
+    cat_photos = glob.glob('/Users/Carlos/Projects/Data_Mining/imageProcessing_2/photos/cats' + '/*.jpg')
+    dog_photos = glob.glob('/Users/Carlos/Projects/Data_Mining/imageProcessing_2/photos/dogs' + '/*.jpg')
     
     #sort photos
     cat_photos.sort()
     dog_photos.sort()
+    # print(cat_photos)
 
     # one big list
     cats_and_dogs = cat_photos + dog_photos
@@ -21,8 +23,11 @@ def main():
     cd_list = list(indexed_cd.items())
     
     randomizer(cd_list, len(indexed_cd))
-    print(cd_list)
+    # print(cd_list)
 
+    #create a folder for every 20 images we have in the list.
+
+    folding(cd_list, 5)
 
 
 
@@ -31,7 +36,32 @@ def randomizer(arr, n):                 #fisher yates algorithm
     for i in range(n-1, 0, -1):
         j = random.randint(0, i+1)
         arr[i], arr[j] = arr[j], arr[i]
-    return(arr)
+    return arr
+
+def folding(cd_list, num):
+    avg = len(cd_list) / float(num)
+    output = []
+    last = 0.0
+
+    # i = 1
+    while last < len(cd_list):
+        output.append(cd_list[int(last):int(last + avg)])
+        # print(len(output))
+        # dest = os.mkdir('/Users/Carlos/Projects/Data_Mining/imageProcessing_2/photos/fold_' + str(i) + '/')
+        # for image in output:
+        #     shutil.move(image, dest)
+        last += avg
+        # i += 1
+    
+    print(len(output))
+    i = 0
+
+    # for fold in range(1, output+1):
+    #     fold_+i= list(fold_+str(i)).append
+
+    # for image in output:
+    #     shutil.move(image, dest)
+    # return output
 
 if __name__ == '__main__':
     main()
