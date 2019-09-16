d09f6f1d180afe2e1c6870b29fdc66f8b8c53e67
Carlos Samaniego <cscarlossamaniego@gmail.com>
2019-09-16 13:01:59 -0400

    fixed indices to process later

--- image_processing.py
+++ image_processing.py
@@ -2,13 +2,11 @@ import glob
 import random
 import os
 import shutil
-from PIL import Image
-import numpy as np
 
 def main():
     #collect list of file paths from cats and dogs
-    cat_photos = glob.glob('/Users/Carlos/Projects/Data_Mining/imageProcessing_2/photos/cats' + '/*.jpg')
-    dog_photos = glob.glob('/Users/Carlos/Projects/Data_Mining/imageProcessing_2/photos/dogs' + '/*.jpg')
+    cat_photos = glob.glob('/Users/Carlos/Projects/Dogs_vs_Cats/photos/cats' + '/*.jpg')
+    dog_photos = glob.glob('/Users/Carlos/Projects/Dogs_vs_Cats/photos/dogs' + '/*.jpg')
     
     #sort photos
     cat_photos.sort()
@@ -19,7 +17,7 @@ def main():
     cats_and_dogs = cat_photos + dog_photos
 
     #created a dictionary with index as value
-    indexed_cd = dict(zip(cats_and_dogs,range(len(cats_and_dogs))))
+    indexed_cd = dict(zip(range(len(cats_and_dogs)), cats_and_dogs))
     # print(indexed_cd)
     
     #create a list of dictionary items
@@ -32,9 +30,6 @@ def main():
     #create a folder for every 20 images we have in the list.
 
     folding(cd_list, 5)
-
-    im = Image.open('images/dot.jpg')
-    iar = np.asarray(im)
     # list(im.getdata()) without numpy
     
 def randomizer(arr, n):                 #fisher yates algorithm
@@ -47,33 +42,11 @@ def folding(cd_list, num):
     avg = len(cd_list) / float(num)
     output = []
     last = 0.0
-    # print(num)
     
-    # for i in range(1,num+1):
-    #     os.mkdir('/Users/Carlos/Projects/Data_Mining/imageProcessing_2/fold_' + str(i) + '/')
-
-    # i = 1
     while last < len(cd_list):
-        twenty = cd_list[int(last):int(last + avg)]
-        for i in twenty:
-            shutil.move(i, '/Users/Carlos/Projects/Data_Mining/imageProcessing_2/fold_' + str(i) + '/')
-
         output.append(cd_list[int(last):int(last + avg)])
-        # print(len(output))
-        # dest = os.mkdir('/Users/Carlos/Projects/Data_Mining/imageProcessing_2/photos/fold_' + str(i) + '/')
-        # for image in output:
-        #     shutil.move(image, dest)
         last += avg
-        # i += 1
-
-    
-    # for fold in output:
-    #     print(fold)
-        # fold_+i= list(fold_+str(i)).append
-
-    # for image in output:
-    #     shutil.move(image, dest)
-    # return output
+    print(output)
 
 if __name__ == '__main__':
     main()
