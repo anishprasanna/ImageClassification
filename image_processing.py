8a819666f884e5283a116f62779bb7b2c9cc932e
Carlos Samaniego <cscarlossamaniego@gmail.com>
2019-09-16 22:22:49 -0400

    working on copying files into fold directories

--- image_processing.py
+++ image_processing.py
@@ -26,6 +26,8 @@ def main():
     #create a folder for every 20 images we have in the list.
     folding(cd_list, 5)
 
+    x_validation(fold_1, fold_2, fold_3, fold_4, fold_5)
+
     
 def randomizer(arr, n):                 #fisher yates algorithm
     for i in range(n-1, 0, -1):
@@ -45,25 +47,49 @@ def folding(cd_list, num):
         output.append(cd_list[int(last):int(last + avg)])
         last += avg
 
+    global fold_1
+    fold_1 = []
+
+    global fold_2
+    fold_2 = []
+
+    global fold_3
+    fold_3 = []
+
+    global fold_4
+    fold_4 = []
+
+    global fold_5
+    fold_5 = []
+
     i = 0
     for l1sts in output:
         for photo in l1sts:
             if i in range(0,20):
+                fold_1.append(photo)
                 shutil.move(photo[1], '/Users/Carlos/Projects/Dogs_vs_Cats/photos/fold_1')
             
             if i in range(20,40):
+                fold_2.append(photo)
                 shutil.move(photo[1], '/Users/Carlos/Projects/Dogs_vs_Cats/photos/fold_2')
 
             if i in range(40, 60):
+                fold_3.append(photo)
                 shutil.move(photo[1], '/Users/Carlos/Projects/Dogs_vs_Cats/photos/fold_3')
             
             if i in range(60, 80):
+                fold_4.append(photo)
                 shutil.move(photo[1], '/Users/Carlos/Projects/Dogs_vs_Cats/photos/fold_4')
             
             if i in range(80, 100):
+                fold_5.append(photo)
                 shutil.move(photo[1], '/Users/Carlos/Projects/Dogs_vs_Cats/photos/fold_5')
             i += 1
 
-    return output
+    return output, fold_1, fold_2, fold_3, fold_4, fold_5
+
+def x_validation(fold_1, fold_2, fold_3, fold_4, fold_5):
+    print(fold_1)
+
 if __name__ == '__main__':
     main()
