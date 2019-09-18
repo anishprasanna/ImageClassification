da527bf6e4df8b5bbb888cbdb1deb2e513312e89
ahais777 <ahaisfield@Andrews-MacBook-Pro.local>
2019-09-18 16:11:41 -0400

    now gets best k-value from k = [1, 10]

--- knn.py
+++ knn.py
@@ -22,70 +22,81 @@ def getAccuracy(testSet, predictions):
 
 def main():
 
-    k = input("What value of k would you like to use? ")
 
-    value = 0
-    #for each row in testData
-    print("Making predictions for each element in testData (this may take a while)...")
-    while (value < len(testData)):
-        #empty array of distances of trainingData from instance in testData
-        distances = np.empty(len(trainingData), dtype=float)
-        testAttribute1 = testData.loc[value, 'Attribute1'].item()
-        testAttribute2 = testData.loc[value, 'Attribute2'].item()
-        testAttribute3 = testData.loc[value, 'Attribute3'].item()
-        num = 0
-        #for each row in trainingData
-        for item in trainingData.iterrows():
-            #calculate distances and store them in distances array
-            trainingAttribute1 = trainingData.loc[num, 'Attribute1'].item()
-            trainingAttribute2 = trainingData.loc[num, 'Attribute2'].item()
-            trainingAttribute3 = trainingData.loc[num, 'Attribute3'].item()
-            distances[num] = euclideanDistance(testAttribute1, testAttribute2, trainingAttribute1, trainingAttribute2, testAttribute3, trainingAttribute3)
-            num += 1
-        #getting indexes of the k closest neighbors and store in indexes array
-        index = 0
-        indexes = np.empty(k, dtype=int)
-        while (index < k):
-            smallest = distances.min()
-            location = np.where(distances == smallest)
+    accurrayForEachK = np.empty(10, dtype=float)
 
-            testTuple = location[0]
-            # if there is a tie
-            if (len(location[i]) > 1):
-                testList = list(testTuple)
-                testList.pop(1)
-                testTuple = tuple(testList)
-            indexes[index] = int(testTuple[0])
-            index += 1
-            distances = distances[distances != smallest]
+    k = 1
+    print("Finding best k-value from 1-10")
+    while (k <= 10):
+        value = 0
+        #for each row in testData
+        print("Making predictions for each element in testData (this may take a while)...")
+        while (value < len(testData)):
+            #empty array of distances of trainingData from instance in testData
+            distances = np.empty(len(trainingData), dtype=float)
+            testAttribute1 = testData.loc[value, 'Attribute1'].item()
+            testAttribute2 = testData.loc[value, 'Attribute2'].item()
+            testAttribute3 = testData.loc[value, 'Attribute3'].item()
+            num = 0
+            #for each row in trainingData
+            for item in trainingData.iterrows():
+                #calculate distances and store them in distances array
+                trainingAttribute1 = trainingData.loc[num, 'Attribute1'].item()
+                trainingAttribute2 = trainingData.loc[num, 'Attribute2'].item()
+                trainingAttribute3 = trainingData.loc[num, 'Attribute3'].item()
+                distances[num] = euclideanDistance(testAttribute1, testAttribute2, trainingAttribute1, trainingAttribute2, testAttribute3, trainingAttribute3)
+                num += 1
+            #getting indexes of the k closest neighbors and store in indexes array
+            index = 0
+            indexes = np.empty(k, dtype=int)
+            while (index < k):
+                smallest = distances.min()
+                location = np.where(distances == smallest)
 
-        cat=0
-        dog=0
-        value1 = 0
-        #seeing if the k closest neighbors are cats or dogs
-        for element in indexes:
-            if (trainingData.loc[indexes[value1], 'Class'].item() == 1.0):
-                cat += 1
+                testTuple = location[0]
+                # if there is a tie
+                if (len(location[i]) > 1):
+                    testList = list(testTuple)
+                    testList.pop(1)
+                    testTuple = tuple(testList)
+                indexes[index] = int(testTuple[0])
+                index += 1
+                distances = distances[distances != smallest]
+
+            cat=0
+            dog=0
+            value1 = 0
+            #seeing if the k closest neighbors are cats or dogs
+            for element in indexes:
+                if (trainingData.loc[indexes[value1], 'Class'].item() == 1.0):
+                    cat += 1
+                else:
+                    dog += 1
+                value1 += 1
+            #print("Cat: ", cat)
+            #print("Dog: ", dog)
+            #using above results, predict the test instance to be a cat or dog
+            if (cat > dog):
+                predictions[value] = 1.0
+            elif (cat < dog):
+                predictions[value] = -1.0
             else:
-                dog += 1
-            value1 += 1
-        #print("Cat: ", cat)
-        #print("Dog: ", dog)
-        #using above results, predict the test instance to be a cat or dog
-        if (cat > dog):
-            predictions[value] = 1.0
-        elif (cat < dog):
-            predictions[value] = -1.0
-        else:
-            predictions[value] = trainingData.loc[indexes[0], 'Class'].item()
-        #print(value)
-        value += 1
-        print(str(value) + " out of " + str(len(testData)) + " completed")
+                predictions[value] = trainingData.loc[indexes[0], 'Class'].item()
+            #print(value)
+            value += 1
+            print(str(value) + " out of " + str(len(testData)) + " completed")
 
-    #print('Predictions: ' + str(predictions))
-    #displays accuracy of predictions made
-    print('Prediction accuracy: ' + str(getAccuracy(testData, predictions)) + "%")
+        #print('Predictions: ' + str(predictions))
+        #displays accuracy of predictions made
+        print('Prediction accuracy: ' + str(getAccuracy(testData, predictions)) + "%")
+        accurrayForEachK[k - 1] = getAccuracy(testData, predictions)
 
+        k += 1
+
+    #returning the best k-value
+    highestAccuracyPercentage = accurrayForEachK.max()
+    bestK = (np.where(accurrayForEachK == highestAccuracyPercentage))
+    print('Best k-value = ' , bestK[0][0] + 1)
 
 
 main()
